bae8962e9f51fabfc2b4a41e1f54679598d1c5f3
PauloRoberto <paulo.rosantos@rede.ulbra.br>
2025-10-20 20:43:28 -0300

    adicionado searchBar

--- lib/injection/injection.dart
+++ lib/injection/injection.dart
@@ -4,15 +4,32 @@ import 'package:ap2/controller/noticia_controller.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
-const search = 'bitcoin';
 
-const baseUrl = 'https://newsapi.org/v2/everything?q=$search&apiKey=$apiKey';
+// valor dinâmico que você vai setar antes de chamar noticiaModule()
+String search = '';
+
+String baseUrl(String s) =>
+    'https://newsapi.org/v2/everything?q=$s&apiKey=$apiKey';
 
 void mainModule() {
   noticiaModule();
 }
 
 void noticiaModule() {
-  getIt.registerLazySingleton<Api>(() => Api(search: search, baseUrl: baseUrl));
-  getIt.registerLazySingleton<NoticiaController>(() => NoticiaController(api: getIt<Api>()));
+  // Remove instâncias antigas antes de registrar de novo
+  if (getIt.isRegistered<Api>()) {
+    getIt.unregister<Api>();
+  }
+  if (getIt.isRegistered<NoticiaController>()) {
+    getIt.unregister<NoticiaController>();
+  }
+
+  // Registra de novo com o valor atualizado
+  getIt.registerLazySingleton<Api>(
+        () => Api(search: search, baseUrl: baseUrl(search)),
+  );
+
+  getIt.registerLazySingleton<NoticiaController>(
+        () => NoticiaController(api: getIt<Api>()),
+  );
 }
--- lib/pages/lista_page.dart
+++ lib/pages/lista_page.dart
@@ -4,8 +4,11 @@ import 'package:ap2/model/noticia.dart';
 import 'package:ap2/widgets/noticia_card.dart';
 import 'package:flutter/material.dart';
 
+import '../api/api.dart';
+
 class ListaPage extends StatefulWidget {
   const ListaPage({super.key});
+  final corpo = true;
 
   @override
   State<ListaPage> createState() => _ListaPageState();
@@ -13,8 +16,14 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
   bool pesquisando = false;
+  final TextEditingController _searchController = TextEditingController();
 
   final controller = getIt.get<NoticiaController>();
+
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   void retornaLista() async {
     final noticias = await controller.buscarNoticias();
@@ -34,24 +43,58 @@ class _ListaPageState extends State<ListaPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: pesquisando
-          ? AppBar(leading: Text('Barra de pesquisa'), actions: [IconButton(onPressed: () {
+      appBar: AppBar(
+        title: pesquisando
+            ? TextField(
+          controller: _searchController,
+          autofocus: true,
+          decoration: const InputDecoration(
+            hintText: 'Pesquisar...',
+            border: InputBorder.none,
+          ),
+          onSubmitted: (texto) async {
+            search = texto.trim();
+
+            // Atualiza instância da API com novo search
+            getIt.unregister<Api>();
+            getIt.registerLazySingleton<Api>(() => Api(
+              search: search,
+              baseUrl: baseUrl(search),
+            ));
+
+            // Recria controller dependente da Api
+            getIt.unregister<NoticiaController>();
+            getIt.registerLazySingleton<NoticiaController>(
+                    () => NoticiaController(api: getIt<Api>()));
+
+            // Busca a nova lista com o termo digitado
+            final noticias = await getIt<NoticiaController>().buscarNoticias();
             setState(() {
+              lista = noticias;
               pesquisando = false;
+              _searchController.clear();
             });
-          }, icon: Icon(Icons.close))],)
-          : AppBar(
-              title: Text('Notícias !!!!!'),
-              actions: [IconButton(onPressed: () {
-                setState(() {
-                  pesquisando = true;
-                });
-              }, icon: Icon(Icons.search))],
-            ),
+          },
+        )
+            : const Text('Now News!'),
+        actions: [
+          IconButton(
+            icon: Icon(pesquisando ? Icons.close : Icons.search),
+            onPressed: () {
+              setState(() {
+                pesquisando = !pesquisando;
+                if (!pesquisando) {
+                  _searchController.clear();
+                }
+              });
+            },
+          ),
+        ],
+      ),
+
       body: SafeArea(
         child: Column(
           children: [
-            pesquisando ? Text('teste') :
             Expanded(
               child: ListView.builder(
                 itemCount: lista.length,
